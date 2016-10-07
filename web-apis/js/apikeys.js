979a327ddd05b2b85a5aec20e95a665669a49f55
zmeier <meier241@umn.edu>
2016-10-06 22:04:34 -0500

    Delete apikeys.js

--- web-apis/js/apikeys.js
+++
@@ -1,9 +0,0 @@
-// Foursquare API access secrets
-var FOURSQUARE_CLIENT_ID = "";
-var FOURSQUARE_CLIENT_SECRET = "";
-
-// OpenWeatherMap API key
-var OPENWEATHERMAP_API_KEY = "";
-
-// News Search API key
-var BINGNEWSSEARCH_API_KEY = "";
